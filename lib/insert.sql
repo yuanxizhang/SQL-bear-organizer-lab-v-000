ac1f8dd7955626f59a16415905769cdfe397c803
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-10 18:03:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,5 @@
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
