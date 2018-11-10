326503440a75152a9575a4f12cfd838ff6a1eda5
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-10 18:37:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "quiet", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "quiet", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Randy", 4, "male", "black", "loud", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Tabitha",  5, "female", "brown", "quiet", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",  7, "male", "brown", "quiet", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",  7, "male", "brown", "quiet", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Melissa",  4, "female", "brown", "quiet", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "male", "black", "quiet", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Wendy",  8, "female", "brown", "quiet", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "female", "brown", "quiet", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "female", "brown", "quiet", 0);
