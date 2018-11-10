34cad2969199730ac73f45e74f7b8db820b490e0
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-10 18:56:39 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,9 +23,9 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT temperament, COUNT(id) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears wWHERE name IS NULL;"
 end
