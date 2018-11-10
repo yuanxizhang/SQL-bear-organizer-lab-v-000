11bea1431c155e79b33c48d710332236fdc230c1
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-10 18:49:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name age FROM bears WHERE gender='female';"
+  "SELECT name, age FROM bears WHERE gender='female';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -7,11 +7,11 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name age FROM bears WHERE alive=1 ORDER BY age;"
+  "SELECT name, age FROM bears WHERE alive=1 ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name age FROM bears ORDER BY age DESC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
