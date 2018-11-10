8542c8087e4a44c51b986625bcea1ee7c7e58baa
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-10 17:59:52 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT DEFAULT 'no name',
-  age INTEGER,
-  gender TEXT,
+  age INTEGER DEFAULT 'not available',
+  gender TEXT ,
   color TEXT,
   temperament TEXT,
   alive INTEGER CHECK (alive < 2)
