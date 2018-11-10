71ab31a33c96447d3b9ff01d648200417805719b
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-10 18:48:10 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Randy", 4, "male", "black", "loud", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Tabitha",  5, "female", "brown", "quiet", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",  7, "male", "brown", "quiet", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Melissa",  4, "female", "brown", "quiet", 1);
